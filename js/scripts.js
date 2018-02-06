968096860e71757195a713c351c7fe95a95065bf
Wojciech Kalebasiak <kalebasiakw@gmail.com>
2018-02-06 10:23:22 +0100

    Add js script

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var a = prompt("Podaj a: "),
+	h = prompt("Podaj h: "),
+	triangleArea;
+triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a 'and height h: ' + h 'is equal to: ' + triangleArea);
